c1f152c20688697abfdcdb0d17a6947e3abdd706
kaykyz2 <kaykytw@gmail.com>
2024-06-10 13:49:02 -0300

    Adicionado o VLibras ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Slidebar from './components/SideBar';
 import { useEffect, useState } from "react";
 
 
+
 function App() {
   const [windowSize, setWindowSize] = useState(window.innerWidth);
 
@@ -23,6 +24,12 @@ function App() {
   return (
     <div className='App'>
     {/* Tudo que estiver fora de outlet, será persistido em todas as telas/rotas do projeto */}
+
+   
+
+    
+
+   
 
     <Slidebar windowSize={windowSize} />
     {/* Tudo dentro de Outlet, será considerado children, declarado na main.jsx, e será reutilizado as rotas com base nisso */}
