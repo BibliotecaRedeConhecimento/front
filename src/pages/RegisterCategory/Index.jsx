c405f02bff229b2f1bc87ff2800c2ef76ad86698
PedroHenriqueBRibeiro <phribeiro2001ph@gmail.com>
2024-06-14 09:09:29 -0300

    inputs dominio e categoria

--- src/pages/RegisterCategory/Index.jsx
+++ src/pages/RegisterCategory/Index.jsx
@@ -1,15 +1,14 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 
 import PageContainer from "../../components/PageContainer";
 import PageHeaderContainer from "../../components/PageHeaderContainer";
 import PageContentContainer from "../../components/PageContentContainer";
 import { ContainerWithSidebar } from "../../components/ContainerWithSidebar";
 
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { MdOutlineAddCircle } from "react-icons/md";
 
-import { Form } from "react-bootstrap"
+import { Form, Row, Col } from "react-bootstrap";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -20,37 +19,49 @@ import ButtonConfirmRegistration from "../../components/ButtonConfirmRegistratio
 
 import { addCategory } from "../../servicesBack/CategoryServices";
 
-
-function RegisterCategory({
+const RegisterCategory = ({
   HandledarkMode,
   isDarkMode,
   decreaseFontSize,
   increaseFontSize,
   logOut,
-}) {
+}) => {
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({ categoryName: "" });
+  const [validated, setValidated] = useState(false);
 
-    const [showModal, setShowModal] = useState(false);
-    const [categoryName, setCategoryName] = useState()
-    const handleOpenModal = () => { setShowModal(true);};
-  
-    const handleCloseModal = () => {
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleOpenModal = () => {
+    const form = document.getElementById("categoryForm");
+    if (form.checkValidity() === false) {
+      setValidated(true);
+      toast.error("O campo é obrigatório!");
+    } else {
+      setShowModal(true);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    toast.error("Cadastro de categoria cancelado.");
+  };
+
+  const registerCategory = async (event) => {
+    event.preventDefault();
+    const response = await addCategory({
+      name: formData.categoryName,
+    });
+    if (response) {
+      toast.success("Categoria cadastrada com sucesso!");
       setShowModal(false);
-      toast.error("Cadastro de categoria cancelado.");
-    };
-  
-    const registerCategory = async (event) => {
-      event.preventDefault();
-      const response = await addCategory({
-        name: categoryName,
-      });
-      console.log(response);
-      if (response) {
-        toast.success("Categoria cadastrada com sucesso!");
-        setShowModal(false);
-      } else {
-        toast.error("Erro ao cadastrar a categoria.");
-      }
-    };
+    } else {
+      toast.error("Erro ao cadastrar a categoria.");
+    }
+  };
 
   return (
     <ContainerWithSidebar
@@ -61,52 +72,63 @@ function RegisterCategory({
       logOut={logOut}
     >
       <PageContainer>
-        <PageHeaderContainer icon={<MdOutlineAddCircle style={{width: 34, marginRight: 5}} />} title={`Cadastrar Categoria`} />
+        <PageHeaderContainer
+          icon={<MdOutlineAddCircle style={{ width: 34, marginRight: 5 }} />}
+          title={`Cadastrar Categoria`}
+        />
         <PageContentContainer>
-
-        <Form.Group controlId="NameCategory">
-              <Form.Label>Nome</Form.Label>
-              <Form.Control type="string"
-              placeholder="Insira a categoria"
-              required
-              />
-             
-            </Form.Group>
-
+          <Form id="categoryForm" noValidate validated={validated} onSubmit={registerCategory}>
+            <Row>
+              <Col md={12}>
+                <Form.Group controlId="categoryName" className="mb-3">
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Insira a categoria"
+                    value={formData.categoryName}
+                    onChange={handleChange}
+                    required
+                    isInvalid={validated && !formData.categoryName}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Campo obrigatório.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+            </Row>
             <div className="d-flex justify-content-between mt-3">
-            <ButtonComponent
-              size="10rem"
-              bgColor="#585859"
-              textColor="white"
-              alternativeText="Voltar"
-            >
-              <IoIosArrowBack style={{ marginRight: 5, width: 12 }} />
-              Voltar
-            </ButtonComponent>
-            <ButtonConfirmRegistration
-              size="10rem"
-              bgColor="#013d32"
-              textColor="white"
-              alternativeText="Cadastrar"
-              action={handleOpenModal}
-            >
-              Cadastrar
-              <IoIosArrowForward style={{ marginLeft: 5, width: 12 }} />
-            </ButtonConfirmRegistration>
-          </div>
-
-            <ModalComponent
-            bodyContent={'Deseja cadastrar a Categoria?'}
+              <ButtonComponent
+                size="10rem"
+                bgColor="#585859"
+                textColor="white"
+                alternativeText="Voltar"
+              >
+                <IoIosArrowBack style={{ marginRight: 5, width: 12 }} />
+                Voltar
+              </ButtonComponent>
+              <ButtonConfirmRegistration
+                size="10rem"
+                bgColor="#013d32"
+                textColor="white"
+                alternativeText="Cadastrar"
+                onClick={handleOpenModal}
+              >
+                Cadastrar
+                <IoIosArrowForward style={{ marginLeft: 5, width: 12 }} />
+              </ButtonConfirmRegistration>
+            </div>
+          </Form>
+          <ModalComponent
+            bodyContent={"Deseja cadastrar a Categoria?"}
             show={showModal}
             handleClose={handleCloseModal}
             confirm={registerCategory}
-            />
-         
-
+          />
         </PageContentContainer>
       </PageContainer>
+      <ToastContainer />
     </ContainerWithSidebar>
   );
-}
+};
 
 export default RegisterCategory;
--- src/pages/RegisterDomain/Index.jsx
+++ src/pages/RegisterDomain/Index.jsx
@@ -1,117 +1,129 @@
-import { React, useState } from "react";
-
+import React, { useState } from 'react';
 import PageContainer from "../../components/PageContainer";
 import PageHeaderContainer from "../../components/PageHeaderContainer";
 import PageContentContainer from "../../components/PageContentContainer";
-import { ContainerWithSidebar } from "../../components/ContainerWithSidebar";
-
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
+import { ContainerWithSidebar } from '../../components/ContainerWithSidebar';
+import { Form, Row, Col } from "react-bootstrap";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { MdOutlineAddCircle } from "react-icons/md";
+import ButtonComponent from '../../components/ButtonBack';
+import ButtonConfirmRegistration from '../../components/ButtonConfirmRegistration';
+import ModalComponent from '../../components/ModalComponent';
+import { addDomain } from '../../servicesBack/DomainServices';
 
-import { Form } from "react-bootstrap"
+const RegisterDomain = ({
+  HandledarkMode,
+  isDarkMode,
+  decreaseFontSize,
+  increaseFontSize,
+  logOut,
+}) => {
+  const [formData, setFormData] = useState({ domainName: "" });
+  const [validated, setValidated] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
 
-import ModalComponent from "../../components/ModalComponent";
-import ButtonComponent from "../../components/ButtonBack";
-import ButtonConfirmRegistration from "../../components/ButtonConfirmRegistration";
+  const handleOpenModal = () => {
+    const form = document.getElementById("domainForm");
+    if (form.checkValidity() === false) {
+      setValidated(true);
+      toast.error("O campo é obrigatório!");
+    } else {
+      setShowModal(true);
+    }
+  };
 
-import { addDomain } from "../../servicesBack/DomainServices";
+  const handleCloseModal = () => {
+    setShowModal(false);
+    toast.error("Cadastro de domínio cancelado.");
+  };
 
+  const registerDomain = async (event) => {
+    event.preventDefault();
+    const response = await addDomain({ name: formData.domainName });
+    if (response) {
+      toast.success("Domínio cadastrado com sucesso!");
+      setShowModal(false);
+    } else {
+      toast.error("Erro ao cadastrar o domínio.");
+    }
+  };
 
-
-function RegisterDomain({
-    HandledarkMode,
-    isDarkMode,
-    decreaseFontSize,
-    increaseFontSize,
-    logOut,
-}) {
-
-    const [showModal, setShowModal] = useState(false);
-    const [domainName, setDomainName] = useState();
-
-    const handleOpenModal = () => {
-        setShowModal(true);
-    };
-
-    const handleCloseModal = () => {
-        setShowModal(false);
-        toast.error("Cadastro de domínio cancelado.");
-    };
-
-    const registerDomain = async (event) => {
-        event.preventDefault();
-        const response = await addDomain({
-            name: domainName,
-        });
-        console.log(response);
-        if (response) {
-            toast.success("Domínio cadastrado com sucesso!");
-            setShowModal(false);
-        } else {
-            toast.error("Erro ao cadastrar o domínio.");
-        }
-    };
-
-    return (
-        <ContainerWithSidebar
-            increaseFontSize={increaseFontSize}
-            decreaseFontSize={decreaseFontSize}
-            HandledarkMode={HandledarkMode}
-            isDarkMode={isDarkMode}
-            logOut={logOut}
-        >
-            <PageContainer>
-                <PageHeaderContainer icon={<MdOutlineAddCircle style={{ width: 34, marginRight: 5 }} />} title={`Cadastrar Domínio`} />
-                <PageContentContainer>
-
-                    <Form.Group controlId="NameDomain">
-                        <Form.Label>Nome</Form.Label>
-                        <Form.Control
-                            type="string"
-                            tabIndex="1"
-                            placeholder="Nome do Domínio"
-                            onChange={(event) => setDomainName(event.target.value)}
-                            required
-                        />
-                    </Form.Group>
-
-                    <div className="d-flex justify-content-between mt-3">
-                        <ButtonComponent
-                            size="10rem"
-                            bgColor="#585859"
-                            textColor="white"
-                            alternativeText="Voltar"
-                        >
-                            <IoIosArrowBack style={{ marginRight: 5, width: 12 }} />
-                            Voltar
-                        </ButtonComponent>
-                        <ButtonConfirmRegistration
-                            size="10rem"
-                            bgColor="#013d32"
-                            textColor="white"
-                            alternativeText="Cadastrar"
-                            action={handleOpenModal}
-                        >
-                            Cadastrar
-                            <IoIosArrowForward style={{ marginLeft: 5, width: 12 }} />
-                        </ButtonConfirmRegistration>
-                    </div>
-
-                    <ModalComponent tabIndex="-1"
-                        bodyContent={"Deseja cadastrar o Domínio?"}
-                        show={showModal}
-                        handleClose={handleCloseModal}
-                        confirm={registerDomain}
+  return (
+    <ContainerWithSidebar
+      increaseFontSize={increaseFontSize}
+      decreaseFontSize={decreaseFontSize}
+      HandledarkMode={HandledarkMode}
+      isDarkMode={isDarkMode}
+      logOut={logOut}
+    >
+      <div>
+        <PageContainer>
+          <PageHeaderContainer
+            icon={<MdOutlineAddCircle style={{ width: 34, marginRight: 5 }} />}
+            title="Cadastrar Domínio"
+          />
+          <PageContentContainer>
+            <Form id="domainForm" noValidate validated={validated} onSubmit={registerDomain}>
+              <Row>
+                <Col md={12}>
+                  <Form.Group controlId="domainName" className="mb-3">
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Nome do Domínio"
+                      value={formData.domainName}
+                      onChange={handleChange}
+                      required
+                      isInvalid={validated && !formData.domainName}
                     />
-
-                </PageContentContainer>
-            </PageContainer>
-        </ContainerWithSidebar>
-    );
-}
+                    <Form.Control.Feedback type="invalid">
+                      Campo obrigatório.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <div className="d-flex justify-content-between mt-3">
+                <ButtonComponent
+                  size="10rem"
+                  bgColor="var(--cinza-primario)"
+                  textColor="white"
+                  alternativeText="Voltar"
+                >
+                  <IoIosArrowBack style={{ marginRight: 5, width: 12 }} />
+                  Voltar
+                </ButtonComponent>
+                <ButtonConfirmRegistration
+                  size="10rem"
+                  bgColor="var(--verde-primario)"
+                  textColor="white"
+                  alternativeText="Cadastrar"
+                  onClick={handleOpenModal}
+                >
+                  Cadastrar
+                  <IoIosArrowForward style={{ marginLeft: 5, width: 12 }} />
+                </ButtonConfirmRegistration>
+              </div>
+            </Form>
+            <ModalComponent
+              tabIndex="-1"
+              bodyContent={"Deseja cadastrar o Domínio?"}
+              show={showModal}
+              handleClose={handleCloseModal}
+              confirm={registerDomain}
+            />
+          </PageContentContainer>
+        </PageContainer>
+        <ToastContainer />
+      </div>
+    </ContainerWithSidebar>
+  );
+};
 
 export default RegisterDomain;
